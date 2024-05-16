1491ca4963aea1ed37e46f80c51036f08843ba46
cgalean0 <cngaleano56@gmail.com>
2024-05-15 21:49:13 -0300

    Rename dirs of practice and add new material of excercise

--- practica#6/better-ticket-machine/TicketMachine.java
+++ practica#6/better-ticket-machine/TicketMachine.java
@@ -20,12 +20,21 @@ public class TicketMachine
 
     /**
      * Create a machine that issues tickets of the given price.
+     * Precondition: The @param cost must be major or equal to zero.
+     * Postcondition: The constructor create a new ticket machine with de @param cost obtained by user.
      */
     public TicketMachine(int cost)
     {
+        if (cost < 0) {
+            throw new IllegalArgumentException("Argument cost invalid!, must be major or equal to zero.");
+        }
         price = cost;
         balance = 0;
         total = 0;
+        assert repOK();
+        assert price == cost;
+        assert balance == 0;
+        assert total == 0;
     }
 
     /**
@@ -48,16 +57,23 @@ public class TicketMachine
     /**
      * Receive an amount of money from a customer.
      * Check that the amount is sensible.
+     * Precondition: The @param amount must be major or equal to zero.
+     * Postcondition: This method insert a new amount in the balance of the User.
      */
     public void insertMoney(int amount)
     {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Argument amount invalid!, must be major or equal to zero.");
+        }
         if(amount > 0) {
             balance = balance + amount;
         }
         else {
-            System.out.println("Use a positive amount rather than: " +
-                               amount);
+            System.out.println("Use a positive amount rather than: " + amount);
         }
+        assert repOK();
+        assert balance == (balance + amount);
+
     }
 
     /**
@@ -67,6 +83,10 @@ public class TicketMachine
      */
     public void printTicket()
     {
+        if (balance <= 0) {
+
+            throw new IllegalStateException("The balance in your account is insuficient!");
+        }
         if(balance >= price) {
             // Simulate the printing of a ticket.
             System.out.println("##################");
@@ -82,21 +102,40 @@ public class TicketMachine
             balance = balance - price;
         }
         else {
-            System.out.println("You must insert at least: " +
-                               (price - balance) + " more cents.");
-                    
+            System.out.println("You must insert at least: " + (price - balance) + " more cents.");
         }
+        assert repOK();
+        assert total == total + price;
+        assert balance > 0;
     }
 
     /**
      * Return the money in the balance.
      * The balance is cleared.
+     * Precondition: The state of balance must be major to zero for refund balance.
+     * Postcondition: Set balance to zero and @return amountToRefund.
      */
     public int refundBalance()
     {
+        if (balance <= 0) {
+            throw new IllegalStateException("You don't have balance to refund :C.");
+        }
         int amountToRefund;
         amountToRefund = balance;
         balance = 0;
+
+        assert repOK();
+        assert balance == 0;
         return amountToRefund;
+    }
+
+    private boolean repOK(){
+        if (price < 0) {
+            return false;
+        }
+        if (balance < 0) {
+            return false;
+        }
+        return true;
     }
 }
